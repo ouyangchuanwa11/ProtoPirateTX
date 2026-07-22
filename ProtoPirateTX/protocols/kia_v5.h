1cbb77b1b78667a227bda12a3cd1ffa74476f72e
ouyangchuanwa11 <ouyangchuanwa@gmail.com>
2026-07-22 18:40:35 +0800

    Final clean: remove ProtoPirateTX/ subfolder, keep sources at root with CI

--- ProtoPirateTX/protocols/kia_v5.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../defines.h"
-#include "../helpers/protopirate_types.h"
-#include <furi.h>
-#include <stdbool.h>
-bool kia_v5_decode(const uint32_t* pulses, uint32_t num_pulses, DecodeResult* result);
-bool kia_v6_decode(const uint32_t* pulses, uint32_t num_pulses, DecodeResult* result);
-bool kia_v7_decode(const uint32_t* pulses, uint32_t num_pulses, DecodeResult* result);
